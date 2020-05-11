695891f0edc0459892260a0349532a7544645e91
lyj168997 <1689978089@qq.com>
2020-05-11 18:05:34 +0800

    返回头部组件

---
+++ src/components/BackHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="headerc">
+        <span @click="goBack">
+            < 返回
+        </span>
+    </header>
+</template>
+<script>
+export default {
+    methods:{
+        goBack(){
+            this.$router.push('/');
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+</style>

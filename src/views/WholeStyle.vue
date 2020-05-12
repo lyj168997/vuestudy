aaf8c59946681337da094ef8a921d26b8505a386
lyj168997 <1689978089@qq.com>
2020-05-12 11:01:54 +0800

    全局引用公共less文件

---
+++ src/views/WholeStyle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        全局引用公共less文件
+        <pre class="pre">
+            (vue-cli3构建的环境)
+            1.安装sass-resources-loader
+            2.在vue.config.js文件中配置
+            const path=require("path");
+            module.exports = {
+                pluginOptions: {
+                    "style-resources-loader": {
+                        preProcessor: "less",
+                        patterns: [path.resolve(__dirname, "less文件路径")]
+                    }
+                }
+            }
+
+        </pre>
+    </div>
+</template>
+<style lang="less" scoped>
+.pre{
+    word-break: break-all;
+    white-space: pre-wrap;
+}
+</style>
